1964e26810add60515d46fc86dcfc6f214ad2f46
Irvan Sandy <irvansandi3@gmail.com>
2025-01-18 12:17:31 +0700

    18 Januari 2025

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["placehold.co"], // Tambahkan domain di sini
+  },
+};
 
 export default nextConfig;
